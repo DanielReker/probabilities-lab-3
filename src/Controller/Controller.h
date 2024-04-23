f7c87dffef4d25a2d8bdc5649842eaacfa439c42
DanielReker <sasha29072004@gmail.com>
2024-04-23 22:05:47 +0400

    Refactored params

--- src/Controller/Controller.h
+++ src/Controller/Controller.h
@@ -22,17 +22,17 @@ private:
 
 	std::unique_ptr<IDistributuion<double>> getDistribution(
 		const std::string& name,
-		const std::map<std::string, double>& params,
+		const auto& params,
 		Seed seed
 	) {
 		// TODO: Refactor dispatching
 		if (name == "exponential") {
-			return std::make_unique<ExponentialDistribution<double>>(seed, params.at("lambda"));
+			return std::make_unique<ExponentialDistribution<double>>(seed, params["lambda"]);
 		} else if (name == "normal") {
-			return std::make_unique<NormalDistribution<double>>(seed, params.at("mu"), params.at("sigma"));
-		} else if (name == "normal_approximated") {
+			return std::make_unique<NormalDistribution<double>>(seed, params["mu"], params["sigma"]);
+		} else if (name == "normalApproximated") {
 			return std::make_unique<ApproximatedNormalDistribution<double>>(
-				seed, params.at("mu"), params.at("sigma"), params.at("n")
+				seed, params["mu"], params["sigma"], params["n"]
 			);
 		} else throw std::invalid_argument("Unknown distribution");
 	}
@@ -52,6 +52,37 @@ private:
 		return seed;
 	}
 
+	std::string formatImageFilePath(
+		const std::string& distribution,
+		Seed seed,
+		int sampleSize,
+		const auto& params
+	) {
+		std::string imgFilePath = std::format(
+			"img/{}_sampleSize{}_seed{}",
+			distribution,
+			sampleSize,
+			seed
+		);
+		for (const auto& [paramName, paramValue] : params.items()) {
+			imgFilePath += std::format("_{}{}", paramName, paramValue.get<double>());
+		}
+		imgFilePath += "/histogram.pdf";
+		return imgFilePath;
+	}
+
+	std::string formatChartTitle(
+		const std::string& distributionName,
+		int sampleSize,
+		const auto& params
+	) {
+		std::string chartTitle = std::format("{}, samples = {}", distributionName, sampleSize);
+		for (const auto& [paramName, paramValue] : params.items()) {
+			chartTitle += std::format(", {} = {}", paramName, paramValue.get<double>());
+		}
+		return chartTitle;
+	}
+
 
 public:
 	Controller(const std::string& configFile) :
@@ -60,29 +91,27 @@ public:
 
 	void execute() {
 		std::string distributionName = m_config["distribution"];
-		std::map<std::string, double> params =
-			m_config["params"][distributionName].get<std::map<std::string, double>>();
 		int sampleSize = m_config["sampleSize"];
 		Seed seed = loadSeed();
 
+		auto params = m_config["params"][distributionName];
 		std::unique_ptr<IDistributuion<double>> distribution = getDistribution(distributionName, params, seed);
+
 		auto sample = generateSample(distribution.get(), sampleSize);
-
-
-		std::string histogramTitle = std::format("{}, samples = {}", distributionName, sampleSize);
-		for (const auto& [paramName, paramValue] : params) {
-			histogramTitle += std::format(", {} = {}", paramName, paramValue);
-		}
-		Histogram histRenderer(sample, [&](double x) { return distribution->probability(x); }, histogramTitle, m_config["binsCoefficient"]);
-
-		std::string imgFilePath = std::format("img/{}_sampleSize{}_seed{}", distributionName, sampleSize, seed);
-		for (const auto& [paramName, paramValue] : params) {
-			imgFilePath += std::format("_{}{}", paramName, paramValue);
-		}
-		imgFilePath += "/histogram.pdf";
+		
+		Histogram histRenderer(
+			sample,
+			[&](double x) { return distribution->probability(x); },
+			formatChartTitle(
+				m_config["distributions"][distributionName]["name"].get<std::string>(),
+				sampleSize,
+				params
+			),
+			m_config["binsCoefficient"]
+		);
 
 		histRenderer.printTable(std::cout, [&](double x) { return distribution->cumulativeProbability(x); });
-		histRenderer.save(imgFilePath);
+		histRenderer.save(formatImageFilePath(distributionName, seed, sampleSize, params));
 		histRenderer.show();
 	}
 };
